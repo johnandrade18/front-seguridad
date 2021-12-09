126cc342aeaec3881c0d528f99dde8392fbb2803
John Andrade <johnandrade18@hotmail.com>
2021-12-09 10:58:15 -0500

    cambio de textos productos

--- pages/productos.js
+++ pages/productos.js
@@ -24,10 +24,10 @@ const Productos = () => {
   return (
     <div>
       <Layout>
-        <h1 className="text-2xl text-white font-light">Productos</h1>
+        <h1 className="text-2xl text-white font-light">Reportes</h1>
         <Link href="/nuevoproducto">
           <a className="bg-blue-800 py-2 px-5 mt-3 inline-block text-white rounded text-sm hover:bg-gray-800 mb-3 uppercase font-bold">
-            Nuevo producto
+            Nuevo Reporte
           </a>
         </Link>
         <div className="overflow-x-scroll">
