19f86ee7da1385c6911514a6176e9888ca2759f3
John Andrade <johnandrade18@hotmail.com>
2021-12-09 10:55:43 -0500

    cambio de textos ciudadanos

--- pages/index.js
+++ pages/index.js
@@ -33,10 +33,10 @@ const Index = () => {
   return (
     <div>
       <Layout>
-        <h1 className="text-2xl text-white font-light">Clientes</h1>
+        <h1 className="text-2xl text-white font-light">Ciudadanos</h1>
         <Link href="/nuevocliente">
           <a className="bg-blue-800 py-2 px-5 mt-3 inline-block text-white round text-sm hover:bg-gray-800 mb-3 uppercase font-bold w-full lg:w-auto text-center">
-            Nuevo cliente
+            Nuevo ciudadano
           </a>
         </Link>
         <div className="overflow-x-scroll">
