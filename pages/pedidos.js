86498d061583a24c94f3c0d74e25d8a863ae1746
John Andrade <johnandrade18@hotmail.com>
2021-12-09 10:56:59 -0500

    cambio textos pedidos

--- pages/pedidos.js
+++ pages/pedidos.js
@@ -39,10 +39,10 @@ const Pedidos = () => {
   return (
     <div>
       <Layout>
-        <h1 className="text-2xl text-white font-light">Pedidos</h1>
+        <h1 className="text-2xl text-white font-light">Entidades</h1>
 
         <Link href="/nuevopedido">  
-          <a className="bg-blue-800 py-2 px-5 mt-3 inline-block text-white round text-sm hover:bg-gray-800 mb-3 uppercase font-bold "> Nuevo Pedido</a>
+          <a className="bg-blue-800 py-2 px-5 mt-3 inline-block text-white round text-sm hover:bg-gray-800 mb-3 uppercase font-bold "> Nuevo Entidad</a>
         </Link> 
 
         { obtenerPedidosVendedor.length === 0 ? (
