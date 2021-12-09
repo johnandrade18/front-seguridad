3731dc9a94b028eb29242a4f4fde129bc391ef56
John Andrade <johnandrade18@hotmail.com>
2021-12-09 10:48:51 -0500

    cambio url conexion

--- config/apollo.js
+++ config/apollo.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 import {setContext} from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-    uri: 'https://api-saint.herokuapp.com/',
+    uri: 'https://agile-retreat-84897.herokuapp.com/',
     fetch
 });
 
